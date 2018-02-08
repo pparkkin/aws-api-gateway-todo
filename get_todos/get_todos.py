9d5655ac5a240a185495947751adb7022552518a
Paavo <pparkkin@gmail.com>
2018-02-08 15:56:17 +0800

    Actually get items from DynamoDB

--- get_todos/get_todos.py
+++ get_todos/get_todos.py
@@ -4,12 +4,14 @@ import boto3
 def get_todos(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('todos')
-    res = [
-        { 'ctime': str(table.creation_date_time) }
-    ]
+    res = table.scan()
+    if res is None:
+        items = []
+    else:
+        items = res.get("Items", [])
     return {
         "isBase64Encoded": False,
         "statusCode": 200,
         # "headers": { "headerName": "headerValue", ... },
-        "body": json.dumps(res)
+        "body": json.dumps(items)
     }
